9e06a0fae7e3605628c26d9db743be0d9056e0d2
Arun Roy <rarunroy6@gmail.com>
2021-05-01 10:06:58 +0800

    first commit

---
+++ src/FirstSteps/HelloWorld.asm
@@ -0,0 +1,24 @@
+section .data   ; initialized data ; constants
+
+    ; labels are memory locations
+    ; msg -> is memory location
+    ; db -> define byte
+    msg: db "Hello World", 10, 0 ; this is our message; 10 -> ascii for newline; 0 -> null at end of message
+
+    msg_L : equ $-msg
+
+
+; print someting
+section .text   ; asm code
+    global _start:
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, msg_L
+    int 80h
+
+    mov eax, 1
+    mov ebx, 0
+    int 80h
